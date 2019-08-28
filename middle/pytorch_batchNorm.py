83a618a4752a47ca718485af05906c6db5f68ee7
hdhcy <982768496@qq.com>
2019-08-28 11:34:17 +0800

    2019/08/28

---
+++ middle/pytorch_batchNorm.py
@@ -0,0 +1,71 @@
+'''
+Author: hdhcy
+Email: 982768496@qq.com
+
+date: 2019/8/28 10:49
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+'''
+Feature scaling
+    Image Normalization
+        normalization=transforms.Normalize(mean=[0.485,0.456,0.406],
+                                           std=[0.229,0.224,0.225])
+    Batch Normalization
+        Batch Norm 
+        Layer Norm
+        Instance Norm
+        Group Norm
+        
+        Advantages
+            Converge faster
+            Better performance
+            Robust
+                stable
+                larger learning rate
+'''
+x=torch.rand(100,16,28,28)
+
+#chanel的数量
+layer=nn.BatchNorm2d(16)
+out=layer(x)
+#u
+print(layer.running_mean)
+#sig
+print(layer.running_var)
+
+
+x=torch.rand(1,16,7,7)
+layer=nn.BatchNorm2d(16)
+out=layer(x)
+print(out.shape)
+
+print(layer.weight)
+print(layer.weight.shape)
+print(layer.bias)
+print(layer.bias.shape)
+print(layer.running_mean)
+print(layer.running_var)
+print(vars(layer))
+layer.eval()
+print(vars(layer))
+print(layer.eval())
+print(vars(layer))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ middle/pytorch_pooling.py
@@ -0,0 +1,37 @@
+'''
+Author: hdhcy
+Email: 982768496@qq.com
+
+date: 2019/8/28 10:20
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+x=torch.rand(1,16,14,14)
+
+#window_size=2 stride=2
+layer=nn.MaxPool2d(2,stride=2)
+
+out=layer(x)
+print(out.shape)
+
+out=F.avg_pool2d(x,2,stride=2)
+print(out.shape)
+
+#upsample 放大
+x=out
+out=F.interpolate(x,scale_factor=2,mode='nearest')
+print(out.shape)
+
+out=F.interpolate(x,scale_factor=3,mode='nearest')
+print(out.shape)
+
+#One unit: conv2d-Biary Normization-Pooling-Relu
+print(x.shape)
+layer=nn.ReLU(inplace=True)
+out=layer(x)
+print(out.shape)
+
+out=F.relu(x)
+print(out.shape)
